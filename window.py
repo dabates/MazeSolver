3519405b09f118374dadcac476a26c38c1a3fad6
David Bates <me@dave-bates.net>
2025-02-21 08:23:59 -0600

    Created new classes - Creates window class - Creates main to create a window

---
+++ window.py
@@ -0,0 +1,26 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width, height):
+        self.root = Tk()
+        self.root.title("Window")
+        self.root.geometry(f"{width}x{height}")
+        self.canvas = Canvas(self.root, width=width, height=height)
+        self.canvas.pack(fill=BOTH, expand=True)
+
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+
+        self.running = False
+
+    def redraw(self):
+        self.root.update_idletasks()
+        self.root.update()
+
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+
+    def close(self):
+        self.running = False
+        self.root.destroy()

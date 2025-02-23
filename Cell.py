f31898b3993639f3256bce0806cf863f58c12bd1
David Bates <me@dave-bates.net>
2025-02-23 07:07:57 -0600

    Add DrawMove - Adds new function to draw the line for the move

--- Cell.py
+++ Cell.py
@@ -16,6 +16,9 @@ class Cell:
         self._win = win
 
     def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
+
         self._x1, self._y1 = x1, y1
         self._x2, self._y2 = x2, y2
 
@@ -27,3 +30,11 @@ class Cell:
             self._win.draw_line(Line(Point(x2, y1), Point(x2, y2)))
         if self.has_bottom_wall:
             self._win.draw_line(Line(Point(x1, y2), Point(x2, y2)))
+
+    def draw_move(self, to_cell, undo=False):
+        draw_color = "gray" if undo else "red"
+
+        start_center = Point(self._x1 + (self._x2 - self._x1) / 2, self._y1 + (self._y2 - self._y1) / 2)
+        end_center = Point(to_cell._x1 + (to_cell._x2 - to_cell._x1) / 2, to_cell._y1 + (to_cell._y2 - to_cell._y1) / 2)
+
+        self._win.draw_line(Line(start_center, end_center), draw_color)
